97ad46188d3cfb143e71f6cf225cc2a496c72340
Roksana Bzińska <roksana.bazinska@gmail.com>
2020-05-04 12:36:09 +0200

    initial commit

---
+++ Homepage/main.js
@@ -0,0 +1,6 @@
+const name = "Filipa";
+const age = 0;
+
+console.log (name);
+console.log (age);
+console.log (`Witam Cię na stronie zawierającej najważniejsze chwile z życia naszego syna ${name}.`);
